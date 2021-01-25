abe10eb6b834df725c4075a2bc28e24c61a8dd46
wangli777 <1151723225@qq.com>
2021-01-25 23:01:07 +0800

    缓存相关

---
+++ src/main/java/com/leenow/demo/config/AppBaseConfiguration.java
@@ -0,0 +1,33 @@
+package com.leenow.demo.config;
+
+import com.leenow.demo.cache.AbstractCacheStore;
+import com.leenow.demo.cache.AbstractStringCacheStore;
+import com.leenow.demo.config.properties.AppConfigProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: LeeNow WangLi
+ * @date: 2021/1/25 22:22
+ * @description:
+ */
+@Configuration
+@EnableConfigurationProperties(AppConfigProperties.class)
+@Slf4j
+public class AppBaseConfiguration {
+
+    @Autowired
+    private AppConfigProperties configProperties;
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AbstractStringCacheStore stringCacheStore() {
+        // TODO: 2021/1/25
+        return null;
+    }
+}
